37f2c8fbcbf48f81b269dafefc07827a4ec744fc
Brandon Dewitt <brandonsdewitt@gmail.com>
2016-11-14 14:57:39 -0700

    add basic specs for pop_up_to

--- spec/multi_op_queue_spec.rb
+++ spec/multi_op_queue_spec.rb
@@ -1,2 +1,39 @@
 require 'spec_helper'
 
+describe ::MultiOpQueue::Queue do
+  before do
+    @queue = ::MultiOpQueue::Queue.new
+  end
+
+  describe "#size" do
+    it "returns 0 when queue is empty" do
+      @queue.size.must_equal 0
+    end
+
+    it "returns the number of items present when queue is not empty" do
+      @queue.push(1)
+      @queue.push(1)
+      @queue.push(1)
+      @queue.push(1)
+      @queue.push(1)
+      @queue.size.must_equal 5
+    end
+  end
+
+  describe "#pop_up_to" do
+    it "returns an array of 1 element when 1 element present" do
+      @queue.push(1)
+      @queue.pop_up_to.must_equal [1]
+    end
+
+    it "returns an array of the pop_up_to number when present" do
+      @queue.push(1)
+      @queue.push(1)
+      @queue.push(1)
+      @queue.push(1)
+      @queue.push(1)
+
+      @queue.pop_up_to(3).must_equal [1, 1, 1]
+    end
+  end
+end
